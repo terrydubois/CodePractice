23cf85e0081a7c10a5d67a6ebc64197ba5bd88d7
Terry DuBois <duboisterrence@gmail.com>
2025-03-11 07:40:45 -0700

    Copy List With Random Pointer

--- CopyListRandomPointer/CopyList.cpp
+++ CopyListRandomPointer/CopyList.cpp
@@ -24,23 +24,33 @@ public:
     }
 };
 
-
-
 Node* copyRandomList(Node* head) {
 
-    // create a mapping of nodes in original to copied nodes
-
+    // map each original node to a new node
     unordered_map<Node*, Node*> map;
-
     Node* current = head;
-    while (head) {
-        
+    while (current) {
+
+        // new node is created with same val as current node
+        map[current] = new Node(current->val);
+        current = current->next;
     }
 
+    // our new nodes do not have their next or random members assigned yet, so let's do that
+    current = head;
+    while (current) {
+        // use the mapped versions of next and random
+        Node* copy = map[current];
+        copy->next = map[current->next];
+        copy->random = map[current->random];
+        current = current->next;
+    }
+
+    return map[head];
 }
 
+// function to print list contents
 void printList(Node* head) {
-
     cout << "printing list...\n";
     while (head != nullptr) {
         cout << "val: " << head->val << ", random: ";
