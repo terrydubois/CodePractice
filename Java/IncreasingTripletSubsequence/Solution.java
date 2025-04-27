b92a3f200fb922a716e82ae84987c7f4da69274f
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2025-04-26 22:02:58 -0700

    Increasing Triplet Subsequence

---
+++ Java/IncreasingTripletSubsequence/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public static boolean increasingTriplet(int[] nums) {
+        
+        // declare two ints, defaulting them to max integer value
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+
+            // a should always be the smallest value we've seen so far
+            if (n < a) {
+                a = n;
+            }
+            // b should be the second smallest value we've seen so far (but still greater than a)
+            else if (n > a && n < b) {
+                b = n;
+            }
+            // if we can find a current int that is greater than b, when b is also greater than a,
+            // then we have found an increasing triplet
+            else if (a < b && b < n) {
+                return true;
+            }
+        }
+        
+        // if we have made it down here, there is no increasing triplet
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] input = {5,4,3,2,1,0};
+        boolean output = increasingTriplet(input);
+        System.out.println("result: " + output);
+    }
+}
